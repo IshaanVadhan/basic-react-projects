ce5478d6b609e42c42eddc077c4a469879b45a0f
Ishaan Vadhan <84273725+IshaanVadhan@users.noreply.github.com>
2022-06-03 15:48:03 +0530

    15th BRP

---
+++ cocktails/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus ea saepe optio repudiandae voluptatibus consequuntur obcaecati porro amet minus, magni dolorum, nulla nobis suscipit a molestias veritatis facere veniam commodi dicta doloremque odio. Quam rerum, in id quis dolorem veniam.
+      </p>
+    </section>
+  );
+};
+
+export default About;
